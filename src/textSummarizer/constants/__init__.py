453279e84bfeb5f66f6b7cacb86361a8d5c21589
dune123 <96974209+dune123@users.noreply.github.com>
2023-11-22 11:56:38 +0530

    model trainer

---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("/Users/sameerraj/Desktop/Machine_learning_python/End-to-End-Text-summarization-project/config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
